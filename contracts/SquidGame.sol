0f211c0ec9a66ab90d7bde3d49480ab49ed2fbc8
lightY <lighty8214@gmail.com>
2021-12-22 23:52:30 +0330

    Update SquidGame contract

--- contracts/SquidGame.sol
+++ contracts/SquidGame.sol
@@ -4,14 +4,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract SquidGame is Context, Ownable {
+        using Counters for Counters.Counter;
+
+        Counters.Counter private _counter;
+
         bool private finished;
         uint256 private registrationFee;
         uint256 private requiredAmount;
 
+        event Claim(address _participant, uint256 _prize, uint8 rank);
+
         struct winners {
-            uint256 rank;
+            uint8 rank;
             uint256 prize;
         }
 
@@ -21,7 +28,7 @@ contract SquidGame is Context, Ownable {
         }
 
         mapping(address => bool) private participants;
-        
+
         mapping(address => bool) private stage1;
         mapping(address => bool) private stage2;
         mapping(address => bool) private stage3;
@@ -38,11 +45,12 @@ contract SquidGame is Context, Ownable {
         //Using this function, participants can register into the event by paying registration fee
         function register() public payable notFinished {
             uint256 transferredAmount = msg.value;
-            require(laqiraToken().balanceOf(_msgSender()) >= requiredAmount &&
-            squidToken().balanceOf(_msgSender()) >= requiredAmount);
-            require(!isParticipant(_msgSender()));
-            require(transferredAmount >= registrationFee);
+            require(laqiraToken().balanceOf(_msgSender()) >= requiredAmount, 'Insufficient LQR balance');
+            require(squidToken().balanceOf(_msgSender()) >= requiredAmount, 'Insufficient SQUID balance');
+            require(!isParticipant(_msgSender()), 'Participant already exists');
+            require(transferredAmount >= registrationFee, 'Insufficient fund for registration');
             participants[_msgSender()] = true;
+            _counter.increment();
         }
 
         //Using the function, event admin can set an entrance fee for participants
@@ -63,31 +71,54 @@ contract SquidGame is Context, Ownable {
 
         //Function is called by the owner, when a participant passes stage 1, successfully
         function setStage1Pass(address _participant) public onlyOwner {
+            require(isParticipant(_participant), 'Given address has not registered');
             stage1[_participant] = true;
         }
 
         //Function is called by the owner, when a participant passes stage 2, successfully
         function setStage2Pass(address _participant) public onlyOwner {
+            require(stage1[_participant], 'Stage1: Wrong participant');
             stage2[_participant] = true;
         }
 
         //Function is called by the owner, when a participant passes stage 3, successfully
         function setStage3Pass(address _participant) public onlyOwner {
+            require(stage2[_participant], 'Stage2: Wrong participant');
             stage3[_participant] = true;
         }
 
         //Function is called by the owner for a winner participant to set his/her rank and prize
-        function setStage4Pass(address _participant, uint256 _rank, uint256 _prize) public onlyOwner {
+        function setStage4Pass(address _participant, uint8 _rank, uint256 _prize) public onlyOwner {
+            require(stage3[_participant], 'Stage3: Wrong participant');
+
             stage4[_participant].rank = _rank;
             stage4[_participant].prize = _prize;
         }
 
+        function batchSetStage(address[] memory _participants, string memory _stageName) public onlyOwner {
+            uint256 i;
+            if (keccak256(bytes(_stageName)) == keccak256('setStage1Pass')) {
+                for (i = 0; i < _participants.length; i++) 
+                    setStage1Pass(_participants[i]);
+            } else if (keccak256(bytes(_stageName)) == keccak256('setStage2Pass')) {
+                for (i = 0; i < _participants.length; i++)
+                    setStage2Pass(_participants[i]);
+            } else if (keccak256(bytes(_stageName)) == keccak256('setStage3Pass')) {
+                for (i = 0; i < _participants.length; i++) 
+                    setStage3Pass(_participants[i]);
+            } else {
+                revert('Wrong stage name');
+            }
+        }
+
         //Using the function, winners can claim their prize
         function claimReward() public {
-            require(stage4[_msgSender()].prize > 0);
+            uint256 prize = stage4[_msgSender()].prize;
+            require(prize > 0, 'Prize must be greater than 0');
             address payable client = payable(_msgSender());
-            client.transfer(stage4[_msgSender()].prize);
+            client.transfer(prize);
             stage4[_msgSender()].prize = 0;
+            emit Claim(_msgSender(), prize, stage4[_msgSender()].rank);
         }
 
         function adminWithdrawal(uint256 _amount) public onlyOwner {
@@ -111,8 +142,24 @@ contract SquidGame is Context, Ownable {
             return _squidToken;
         }
 
-       modifier notFinished() {
-           require(!finished, 'Registration period has ended');
+        function getRegisterdParticipants() public view returns (uint256) {
+            return _counter.current();            
+        }
+
+        function fee() public view returns (uint256) {
+            return registrationFee;
+        }
+
+        function getRequiredAmount() public view returns (uint256) {
+            return requiredAmount;
+        }
+
+        function getParticipantCurrentStage(address _participant) public view returns (bool[] memory currentStage) {
+
+        }
+
+        modifier notFinished() {
+            require(!finished, 'Registration period has ended');
            _;
-       }
+        }
 }
